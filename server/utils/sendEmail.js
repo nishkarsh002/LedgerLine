e1c8466ff907e90bd1a33ab39a5f42a75841ee42
Fire_Sail <81484514+pawansaini29@users.noreply.github.com>
2026-04-20 14:17:18 +0530

    ok

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (options) => {
         console.log(`📡 Attempting to send email to: ${options.email} via Resend API...`);
 
         const response = await axios.post('https://api.resend.com/emails', {
-            from: 'Powerfilling <noreply@powerfilling.com>',
+            from: 'Powerfilling <noreply@powerfiling.com>',
             to: options.email,
             subject: options.subject,
             text: options.message,
